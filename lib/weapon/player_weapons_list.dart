f0fe33724595aa89e6ff16d62fa8a473c26dd0ce
drakkonne <gagind@blastmaker.kg>
2023-06-14 11:21:26 +0600

    Доделал меч

--- lib/weapon/player_weapons_list.dart
+++ lib/weapon/player_weapons_list.dart
@@ -4,7 +4,6 @@ import 'package:flame/flame.dart';
 import 'package:flame/geometry.dart';
 import 'package:flame/sprite.dart';
 import 'package:game_flame/weapon/weapon.dart';
-import 'package:game_flame/components/physic_vals.dart';
 import 'dart:math' as math;
 
 class WSword extends PlayerWeapon
@@ -24,6 +23,9 @@ class WSword extends PlayerWeapon
   int _hitVariant = 0;
   late SpriteAnimation _animShort, _animLong;
   bool _isGrow = true;
+  final double _startAngle = tau - tau/6;
+  double activeSecs = 0;
+  double _diffAngle = 0;
 
   @override
   void onMount()
@@ -57,24 +59,28 @@ class WSword extends PlayerWeapon
       int rand = math.Random().nextInt(2);
       late SpriteAnimationTicker tick;
       if(rand == 0){
-        position = 
+        _diffAngle = 0;
+        angle = _startAngle;
+        size = Vector2(42,10);
         tick = SpriteAnimationTicker(_animShort);
         gameRef.gameMap.orthoPlayer?.animation = _animShort;
       }else{
         tick = SpriteAnimationTicker(_animLong);
         gameRef.gameMap.orthoPlayer?.animation = _animLong;
+        size = Vector2(1,10);
+        angle = 0;
       }
+      debugMode = true;
       gameRef.playerData.energy.value -= energyCost;
       _activeSecs = tick.totalDuration();
       _hitVariant = rand;
       gameRef.playerData.isLockEnergy = true;
-      debugMode = true;
       collisionType = CollisionType.active;
       // print('start hit');
       await Future.delayed(Duration(milliseconds: (_activeSecs * 1000).toInt()),(){
         collisionType = CollisionType.inactive;
         _isGrow = true;
-        // debugMode = false;
+        debugMode = false;
         gameRef.playerData.isLockEnergy = false;
         onEndWeaponHit.call();
         // print('end hit');
@@ -87,17 +93,16 @@ class WSword extends PlayerWeapon
   {
     if(collisionType == CollisionType.active){
       if(_hitVariant == 0){
-        // position = Vector2(position.x + dt/_activeSecs * 50, position.y);
+        if(angle < 8.360913459421951) {
+          _diffAngle += dt / (_activeSecs / 2) * sectorInRadian * 4;
+          angle = _startAngle + _diffAngle;
+        }
       }else{
         if(_isGrow && size.x > _maxLength/2){
           _isGrow = false;
         }
         _isGrow ? size = Vector2(size.x + dt/_activeSecs * _maxLength, size.y) : size = Vector2(size.x - dt/_activeSecs * _maxLength, size.y);
       }
-      //
-      //
-      // _diffAngle -= dt/_activeSecs * sectorInRadian;
-      // angle = _startAngle + _diffAngle;
     }
     super.update(dt);
   }
