03ccc96890578db48bcc95d8c9aa185152ada7ad
drakkonne <gagindaniil@inbox.ru>
2023-06-26 21:34:11 +0600

    Сделал класс, который компилирует ВСЕ анимационные изображения в одно

--- lib/components/MapNode.dart
+++ lib/components/MapNode.dart
@@ -13,10 +13,79 @@ import 'package:game_flame/kyrgyz_game.dart';
 import 'package:game_flame/enemies/grass_golem.dart';
 import 'package:xml/xml.dart';
 
+class MySuperAnimCompiler {
+  Map<SpriteAnimation?, List<Vector2>> _allMap = {};
+  Map<SpriteAnimation?, SpriteAnimationTicker> _tickers = {};
+
+  Future addTile(Vector2 position, TileProcessor tileProcessor) async {
+    var animation = await tileProcessor.getSpriteAnimation();
+    if (animation == null) {
+      return;
+    }
+    if (!_allMap.containsKey(animation)) {
+      _allMap.putIfAbsent(animation, () => []);
+      _tickers.putIfAbsent(animation, () => animation.ticker());
+    }
+    _allMap[animation]!.add(position);
+  }
+
+  Future<void> compile() async {
+    for (int i = 0; i < 16; i++) {
+      final composition = ImageCompositionExt();
+      for (final anim in _allMap.keys) {
+        if (anim == null) {
+          continue;
+        }
+        _tickers[anim]!;
+        List<Sprite> newSprites = [];
+        while (_tickers[anim]!.currentIndex < anim.frames.length) {
+          final sprite = _tickers[anim]!.getSprite();
+          for (final pos in _allMap[anim]!) {
+            composition.add(sprite.image, pos, source: sprite.src);
+          }
+        }
+      }
+      final composedImage = composition.compose();
+      // composedImage.resize(Vector2.all(92000));
+      var byteData = await composedImage.toByteData(format: ImageByteFormat.png);
+      File file = File(i.toString() + '.png');
+      file.writeAsBytesSync(byteData!.buffer.asUint8List());
+      for (final anim in _allMap.keys) {
+        _tickers[anim]!.currentIndex++;
+        if (_tickers[anim]!.currentIndex == anim!.frames.length) {
+          _tickers[anim]!.currentIndex = 0;
+        }
+      }
+    }
+  }
+}
+
+Future<SpriteAnimationComponent> waterElement(List<Vector2> positionsAnim) async
+{
+  final spriteImage = await Flame.images.load('tiles/map/ancientLand/Tilesets/Tileset-Animated Terrains-8 frames.png');
+  final spriteSheet = SpriteSheet(image: spriteImage, srcSize: Vector2(32,32));
+  List<Sprite> newSprites = [];
+  for (int i=0;i<8;i++) {
+    final sprite = spriteSheet.getSprite(1, i);
+    final composition = ImageCompositionExt();
+    for (final pos in positionsAnim) {
+      composition.add(sprite.image, pos, source: sprite.src);
+    }
+    final composedImage = composition.compose();
+    newSprites.add(Sprite(composedImage));
+  }
+  final spriteAnimation = SpriteAnimation.variableSpriteList(newSprites,
+      stepTimes:[0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1]);
+  return SpriteAnimationComponent(
+      animation: spriteAnimation,
+      position: Vector2.all(0),
+      size: newSprites.first.image.size);
+}
+
 class Water extends SpriteAnimationComponent
 {
-  Water(this.pos);
-  Vector2 pos;
+  Water(this.positionsAnim);
+  List<Vector2> positionsAnim;
   @override
   Future<void> onLoad() async
   {
@@ -24,7 +93,7 @@ class Water extends SpriteAnimationComponent
     final spriteSheet = SpriteSheet(image: spriteImage, srcSize: Vector2(32,32));
     animation = spriteSheet.createAnimation(row: 1, stepTime: 0.1, from: 0,to: 8);
     size = Vector2(33, 33);
-    position = pos;
+    position = Vector2.all(0);
   }
 }
 
@@ -48,18 +117,28 @@ class MapNode extends PositionComponent with HasGameRef<KyrgyzGame>
     if(column < 0 || row < 0) {
       return;
     }
+    if(column != 0 && row != 0) {
+      return;
+    }
     isNeedLoadEnemy = !gameRef.gameMap.loadedColumns.contains(column) || !gameRef.gameMap.loadedRows.contains(row);
     _image = await Flame.images.load('0-0.png');
     position = Vector2(column * GameConsts.lengthOfTileSquare, row * GameConsts.lengthOfTileSquare);
-    var fileName = '$column-$row.tmx';
+    // var fileName = '$column-$row.tmx';
+    var fileName = 'top_left_anim.tmx';
+    print(fileName);
     final text = await Flame.assets.readFile(fileName);
     final objects = XmlDocument.parse(text.toString()).findAllElements('object');
+    bool isWater = false;
+    List<Vector2> waterPoses = [];
     for(final obj in objects) {
       switch(obj.getAttribute('name')) {
         case 'enemy':  await createEnemy(obj); break;
-        case 'ground': add(Ground(size: Vector2(double.parse(obj.getAttribute('width')!), double.parse(obj.getAttribute('height')!)),position: Vector2(double.parse(obj.getAttribute('x')!) + column * GameConsts.lengthOfTileSquare, double.parse(obj.getAttribute('y')!) + row * GameConsts.lengthOfTileSquare))); break;
-        case 'water':  add(Water(Vector2(double.parse(obj.getAttribute('x')!) + column * GameConsts.lengthOfTileSquare, double.parse(obj.getAttribute('y')!) + row * GameConsts.lengthOfTileSquare)));
+        case 'ground': await add(Ground(size: Vector2(double.parse(obj.getAttribute('width')!), double.parse(obj.getAttribute('height')!)),position: Vector2(double.parse(obj.getAttribute('x')!), double.parse(obj.getAttribute('y')!)))); break;
+        case 'water':  waterPoses.add(Vector2(double.parse(obj.getAttribute('x')!), double.parse(obj.getAttribute('y')!)));//  await add(Water(Vector2(double.parse(obj.getAttribute('x')!), double.parse(obj.getAttribute('y')!)))); isWater = true; break;
       }
+    }
+    if(waterPoses.length > 0) {
+      add(await waterElement(waterPoses));
     }
   }
 
@@ -69,8 +148,8 @@ class MapNode extends PositionComponent with HasGameRef<KyrgyzGame>
       return;
     }
     await gameRef.gameMap.add(GrassGolem(Vector2(
-      double.parse(obj.getAttribute('x')!) + column * GameConsts.lengthOfTileSquare,
-      double.parse(obj.getAttribute('y')!) + row * GameConsts.lengthOfTileSquare), GolemVariant.Water));
+        double.parse(obj.getAttribute('x')!) + column * GameConsts.lengthOfTileSquare,
+        double.parse(obj.getAttribute('y')!) + row * GameConsts.lengthOfTileSquare), GolemVariant.Water));
   }
 
   @override
