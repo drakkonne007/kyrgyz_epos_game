67c6b7e0895a2ae9e3bfca677c28501879bc6110
drakkonne <gagindaniil@inbox.ru>
2023-12-07 17:05:00 +0600

    Делаю по красоте

--- lib/overlays/dialog_overlay.dart
+++ lib/overlays/dialog_overlay.dart
@@ -25,19 +25,38 @@ class _DialogOverlayState extends State<DialogOverlay> {
               Row(
                   mainAxisSize: MainAxisSize.max,
                   children:[
-                    Image.asset('assets/tiles/sprites/dialogIcons/azura.png',isAntiAlias: true,width: constraints.maxWidth/2,height: constraints.maxHeight/2,fit: BoxFit.contain,),
+                    Image.asset('assets/tiles/sprites/dialogIcons/azura.png',
+                        isAntiAlias: true,
+                        width: constraints.maxWidth/2,
+                        height: constraints.maxHeight/2,
+                        fit: BoxFit.contain,
+                        filterQuality: FilterQuality.high),
                     DialogAnswers(widget._game,Vector2(constraints.maxWidth/2,constraints.maxHeight/2)),
                   ]
               ),
               SizedBox(
                   width: constraints.maxWidth,
                   height: constraints.maxHeight/2,
-                  child: const Text('Вопрос',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold
-                    ),
+                  child:
+                  Stack(
+                      alignment: Alignment.center,
+                      fit: StackFit.passthrough,
+                      children:[
+                        Image.asset(
+                            'assets/images/gui/dialog_field2.png',
+                            width: constraints.maxWidth,
+                            height: constraints.maxHeight/2,
+                            centerSlice: Rect.fromPoints(Offset(75,11), Offset(75+470, 11+180)),
+                            filterQuality: FilterQuality.high
+                        ),
+                        const Text('Вопрос',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold
+                          ),
+                        )
+                      ]
                   )
               )
             ]
@@ -51,7 +70,7 @@ class DialogAnswers extends StatefulWidget
 {
   final KyrgyzGame _game;
   DialogAnswers(this._game, this.size, {super.key});
-  Vector2 size;
+  final Vector2 size;
 
   @override
   State<DialogAnswers> createState() => _DialogAnswersState();
@@ -60,19 +79,47 @@ class DialogAnswers extends StatefulWidget
 class _DialogAnswersState extends State<DialogAnswers> {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-        width: widget.size.x,
-        height: widget.size.y,
-        child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.center,
+    return Stack(
+        alignment: Alignment.center,
+        fit: StackFit.passthrough,
         children:[
-          TextButton(onPressed: widget._game.doGameHud, child: Text('Первый вариант')),
-          TextButton(onPressed: widget._game.doGameHud, child: Text('Второй вариант')),
-          TextButton(onPressed: widget._game.doGameHud, child: Text('Третий вариант')),
-        ])
+          Image.asset(
+              'assets/images/gui/dialog_field2.png',
+              width: widget.size.x,
+              height: widget.size.y,
+              centerSlice: Rect.fromPoints(Offset(75,11), Offset(75+470, 11+180)),
+              filterQuality: FilterQuality.high
+          ),
+          Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children:[
+                TextButton(onPressed: widget._game.doGameHud, child: Text('Первый вариант')),
+                TextButton(onPressed: widget._game.doGameHud, child: Text('Второй вариант')),
+                TextButton(onPressed: widget._game.doGameHud, child: Text('Третий вариант')),
+              ])
+        ]
     );
-    // return
+    //     width: widget.size.x,
+    //     height: widget.size.y,
+    //     child: Column(
+    //     crossAxisAlignment: CrossAxisAlignment.center,
+    //     mainAxisSize: MainAxisSize.max,
+    //     mainAxisAlignment: MainAxisAlignment.center,
+    //     children:[
+    //       TextButton(onPressed: widget._game.doGameHud, child: Text('Первый вариант')),
+    //       TextButton(onPressed: widget._game.doGameHud, child: Text('Второй вариант')),
+    //       TextButton(onPressed: widget._game.doGameHud, child: Text('Третий вариант')),
+    //     ])
+    // );
+    //
+    // Image.asset(
+    // 'assets/images/gui/dialog_field.png',
+    // width: widget.size.x,
+    // height: widget.size.y,
+    // centerSlice: Rect.fromPoints(Offset(214,36), Offset(214 + 1177, 36 + 472)),
+    // );
+    // // return
   }
 }
